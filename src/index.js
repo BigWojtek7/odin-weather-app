135c0c57218fa15664400a306d4ffa28b0cc16b1
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-03-28 17:55:16 +0100

    Add try/catch to fetch

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import "./style.css";
 const form = document.querySelector("form");
 
 async function getWeather(city) {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=1037f513ffb64c10b92100001242603&q=${city}`,
-    { mode: "cors" },
-  );
-  const weatherApi = await response.json();
-  console.log(weatherApi);
-  displayWeather(weatherApi)
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=1037f513ffb64c10b92100001242603&q=${city}`,
+      { mode: "cors" },
+    );
+    const weatherApi = await response.json();
+    console.log(weatherApi);
+    displayWeather(weatherApi);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function submitHandler(e) {
@@ -26,13 +30,13 @@ function displayWeather(weather) {
   const tempDiv = document.querySelector(".temp");
   const skyDiv = document.querySelector(".sky");
 
-  console.log(`wojtek${weather.current.temp_c}`)
-  cityNameDiv.textContent = `${weather.location.name}`
-  tempDiv.textContent = `${weather.current.temp_c}\u2103`
-  skyDiv.innerHTML = `<img src=http:${weather.current.condition.icon}><p>${weather.current.condition.text}</p>`
+  console.log(`wojtek${weather.current.temp_c}`);
+  cityNameDiv.textContent = `${weather.location.name}`;
+  tempDiv.textContent = `${weather.current.temp_c}\u2103`;
+  skyDiv.innerHTML = `<img src=http:${weather.current.condition.icon}><p>${weather.current.condition.text}</p>`;
 }
 
 form.addEventListener("submit", submitHandler);
 
-getWeather("Warsaw")
+getWeather("Warsaw");
 // getWeather();

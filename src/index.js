b0b873f868963e4313ce7acdd624f33fcc560d3a
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-03-28 17:43:18 +0100

    Add function displayWeather

--- src/index.js
+++ src/index.js
@@ -1,24 +1,38 @@
 import "./style.css";
 
 const form = document.querySelector("form");
-const cityInput = document.getElementById("city")
 
-async function getWeather(city){
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=1037f513ffb64c10b92100001242603&q=${city}`, { mode: "cors" });
+async function getWeather(city) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=1037f513ffb64c10b92100001242603&q=${city}`,
+    { mode: "cors" },
+  );
   const weatherApi = await response.json();
   console.log(weatherApi);
-
+  displayWeather(weatherApi)
 }
 
-function submitHandler(e){
+function submitHandler(e) {
+  const cityInput = document.getElementById("city").value;
+
   e.preventDefault();
-  getWeather(cityInput.value)
+  getWeather(cityInput);
 }
 
-// function displayWeather(weather){
-//   ...
-// }
+function displayWeather(weather) {
+  // const weatherContainer = document.querySelector(".weather");
 
-form.addEventListener("submit", submitHandler)
+  const cityNameDiv = document.querySelector(".city");
+  const tempDiv = document.querySelector(".temp");
+  const skyDiv = document.querySelector(".sky");
 
+  console.log(`wojtek${weather.current.temp_c}`)
+  cityNameDiv.textContent = `${weather.location.name}`
+  tempDiv.textContent = `${weather.current.temp_c}\u2103`
+  skyDiv.innerHTML = `<img src=http:${weather.current.condition.icon}><p>${weather.current.condition.text}</p>`
+}
+
+form.addEventListener("submit", submitHandler);
+
+getWeather("Warsaw")
 // getWeather();

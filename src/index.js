7b59e0cd2374b443779630b3c5a8b68dc42e3e70
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-03-28 08:16:41 +0100

    Add function to fetch from weather Api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./style.css";
+
+
+async function getWeather(){
+  const response = await fetch("https://api.weatherapi.com/v1/current.json?key=1037f513ffb64c10b92100001242603&q=warsaw", { mode: "cors" });
+  const weatherApi = await response.json();
+  console.log(weatherApi);
+}
+
+getWeather();

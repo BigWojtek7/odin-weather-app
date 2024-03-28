dec90adf5a4a9519556d388dbdef0f537a4fcdaa
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-03-28 09:01:50 +0100

    Add function submitHandler

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import "./style.css";
 
+const form = document.querySelector("form");
+const cityInput = document.getElementById("city")
 
-async function getWeather(){
-  const response = await fetch("https://api.weatherapi.com/v1/current.json?key=1037f513ffb64c10b92100001242603&q=warsaw", { mode: "cors" });
+async function getWeather(city){
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=1037f513ffb64c10b92100001242603&q=${city}`, { mode: "cors" });
   const weatherApi = await response.json();
   console.log(weatherApi);
+
 }
 
-getWeather();
+function submitHandler(e){
+  e.preventDefault();
+  getWeather(cityInput.value)
+}
+
+// function displayWeather(weather){
+//   ...
+// }
+
+form.addEventListener("submit", submitHandler)
+
+// getWeather();
